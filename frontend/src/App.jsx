34152da2f4e55cd2ccf57213dd5cd7aee356709c
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-15 20:22:40 +0200

    Add redux persist to transfer and keep store states in localstorage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,26 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import MainLayout from './layout/MainLayout'
 import { Provider } from 'react-redux';
-import store from './redux/store/store';
+import store, {persistor} from './redux/store/store';
 import ProductPage from './pages/ProductPage';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Provider store={store}>
-      <Router>
-      <MainLayout>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/women' element={<HomePage category={'Women'}/> }/>
-          <Route path='/product' element={<ProductPage/>} />
-        </Routes>
-      </MainLayout>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+        <MainLayout>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/women' element={<HomePage category={'Women'}/> }/>
+            <Route path='/product' element={<ProductPage/>} />
+          </Routes>
+        </MainLayout>
+        </Router>
+      </PersistGate>
     </Provider>
   )
 }
--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {cart} from '../cart/cartSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { cart } from '../cart/cartSlice';
+
+
+const persistConfig = {
+    key: 'cart', 
+    storage,
+  };
+
+const persistedCartReducer = persistReducer(persistConfig, cart);
+
 
 const store = configureStore({
     reducer: {
-        cart: cart
+        cart: persistedCartReducer
     }
 });
-
+export const persistor = persistStore(store);
 export default store;
 
 
