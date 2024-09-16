1416fabc9761eab77ed9b4812efceb44925a433d
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-16 23:30:02 +0200

    Implement simple login and register

--- frontend/src/components/Auth/Login/LoginBtn.jsx
+++ frontend/src/components/Auth/Login/LoginBtn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const LoginBtn = () => {
+const LoginBtn = ({onClick}) => {
   return (
-    <div className='bg-yellow cursor-pointer p-5 w-[100px] h-[50px] flex items-center justify-center rounded-lg'>
+    <div onClick={onClick} className='bg-yellow cursor-pointer p-5 w-[100px] h-[50px] flex items-center justify-center rounded-lg'>
         <span className='font-bold'>Log in</span>
     </div>
   )
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 import LoginBtn from '../components/Auth/Login/LoginBtn'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const login = async(email, password, navigate) => {
+  try {
+    const request = await axios({
+      method:'post',
+      url: 'http://localhost:3000/api/v1/auth/login',
+      data: {
+        email: email,
+        password: password
+      },
+      withCredentials: true
+    });
+
+    const success = request.data.success;
+    if(success) {
+      navigate('/')
+    }
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+}
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleLogin = () => {
+    login(email,password,navigate);
+  }
   return (
     <div className='w-full h-full flex flex-col sm:mt-5 mt-10 items-center gap-5'>
         <span className='text-3xl font-bold text-blackTheme'>Log in</span>
         <div className='bg-cream h-auto sm:w-[500px] rounded-lg flex flex-col p-10 gap-5'>
             <div className='flex flex-col gap-1'>
                 <span className='text-lg text-blackTheme'>Email</span>
-                <input className='rounded-lg p-2 focus:outline-none' placeholder='johndoe@email.com' type="text" />
+                <input onChange={handleChangeEmail} className='rounded-lg p-2 focus:outline-none' placeholder='johndoe@email.com' type="text" />
             </div>
 
             <div className='flex flex-col gap-1'>
                 <span className='text-lg text-blackTheme'>Password</span>
-                <input className='rounded-lg p-2 focus:outline-none' placeholder='Enter password here' type="password" />
+                <input onChange={handleChangePassword} className='rounded-lg p-2 focus:outline-none' placeholder='Enter password here' type="password" />
             </div>
 
-            <LoginBtn/>
+            <LoginBtn onClick={handleLogin}/>
             <p className='text-sm'>Don't have an account yet? <br />Click <a className='font-bold underline' href="/register">here</a> to register</p>
         </div>
     </div> 
